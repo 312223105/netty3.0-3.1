3a28ca937c734770fa900823b2f8a0294a90ef41
XuZhe <312223105@qq.com>
2018-03-09 19:42:43 +0800

    First server example

--- src/main/java/me/xuzhe/App.java
+++ src/main/java/me/xuzhe/App.java
@@ -1,5 +1,18 @@
 package me.xuzhe;
 
+import net.gleamynode.netty.bootstrap.ServerBootstrap;
+import net.gleamynode.netty.channel.Channel;
+import net.gleamynode.netty.channel.ChannelEvent;
+import net.gleamynode.netty.channel.ChannelFactory;
+import net.gleamynode.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import net.gleamynode.netty.handler.codec.string.StringDecoder;
+import net.gleamynode.netty.pipeline.Pipe;
+import net.gleamynode.netty.pipeline.PipeHandler;
+import net.gleamynode.netty.pipeline.Pipeline;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  *
@@ -8,6 +21,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ChannelFactory factory = new NioServerSocketChannelFactory(
+                Executors.newCachedThreadPool(), Executors.newCachedThreadPool()
+        );
+
+        ServerBootstrap bootstrap = new ServerBootstrap(factory);
+
+        Pipeline<ChannelEvent> pipeline =  bootstrap.getPipeline();
+
+        pipeline.addLast("handler", new StringDecoder());
+
+        bootstrap.setOption("child.tcpNoDelay", true);
+        bootstrap.setOption("child.keepAlive", true);
+        bootstrap.bind(new InetSocketAddress(8080));
+
     }
 }
